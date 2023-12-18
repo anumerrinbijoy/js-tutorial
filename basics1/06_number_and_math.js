cc79b2eae9c7b648651e4e9391ff1566bea91408
anumerrinbijoy <153586599+anumerrinbijoy@users.noreply.github.com>
2023-12-18 11:35:54 +0000

    Math and Num

---
+++ basics1/06_number_and_math.js
@@ -0,0 +1,26 @@
+const score =400
+const balance = new Number(100)
+console.log(score)
+console.log(balance.toString().length)
+console.log(balance.toFixed(1))
+const otherNumber =23.8966
+
+console.log(otherNumber.toPrecision(1))
+
+const hundreds =100000
+console.log(hundreds.toLocaleString('en-IN'))
+
+////+++++++++++++++++++Maths++++++++++++++++++++++++++++//
+console.log(Math)
+console.log(Math.abs(-4))
+console.log(Math.round(-4.8))
+console.log(Math.ceil(4.2))
+console.log(Math.min(4,3,6,8))
+console.log(Math.max(4,3,6,8))
+
+console.log(Math.random())
+console.log(Math.floor(Math.random()*10 )+ 1 )
+
+const min = 10
+const max = 20
+
