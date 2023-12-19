90680762e1eb43f9e2598302905b7e35b4f44fa9
anumerrinbijoy <153586599+anumerrinbijoy@users.noreply.github.com>
2023-12-19 07:05:57 +0000

    Date time formats

---
+++ basics1/07_date_time.js
@@ -0,0 +1,21 @@
+//Dates
+let myDate = new Date()
+console.log(myDate)
+console.log(myDate.toString())
+console.log(myDate.toDateString())
+console.log(myDate.toISOString())
+console.log(myDate.toJSON())
+
+//let mycdate = new Date(2023,1,23,5,3)
+//console.log(mycdate.toString())
+
+let mycdate = new Date("01-14-2023")
+console.log(mycdate.toString())
+
+let mytimestamp = Date.now()
+console.log(mytimestamp.toString())
+
+let newDate =new Date()
+console.log(newDate)
+console.log(newDate.getDate())
+console.log(newDate.getMonth())
